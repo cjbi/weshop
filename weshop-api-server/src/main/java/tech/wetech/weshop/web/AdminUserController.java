f8e21a5518f4e88119759b45d6343100173f8a1f
Jinbao Cheng <cjbi@outlook.com>
2018-11-18 08:52:16 +0800

    代码提交

--- weshop-api-server/src/main/java/tech/wetech/weshop/web/AdminUserController.java
+++ weshop-api-server/src/main/java/tech/wetech/weshop/web/AdminUserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tech.wetech.weshop.core.utils.Reflections;
 import tech.wetech.weshop.core.utils.Result;
 import tech.wetech.weshop.domain.UserLevel;
 import tech.wetech.weshop.enums.GenderEnum;
