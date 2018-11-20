8bbe94780e9415e396db01523cf6fa25a0653345
Jinbao Cheng <cjbi@outlook.com>
2018-11-20 17:09:00 +0800

    用户管理后台功能实现

---
+++ weshop-api-server/src/main/java/tech/wetech/weshop/mapper/CollectMapper.java
@@ -0,0 +1,7 @@
+package tech.wetech.weshop.mapper;
+
+import tech.wetech.weshop.core.utils.MyMapper;
+import tech.wetech.weshop.po.Collect;
+
+public interface CollectMapper extends MyMapper<Collect> {
+}
