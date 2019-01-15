5e46285ff5d47a7a1d0bcd8647be185c352fbc11
Jinbao Cheng <cjbi@outlook.com>
2019-01-15 23:32:32 +0800

    代码提交

--- weshop-server/src/main/java/tech/wetech/weshop/event/FootprintListener.java
+++ weshop-server/src/main/java/tech/wetech/weshop/event/FootprintListener.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 import tech.wetech.weshop.mapper.FootprintMapper;
 import tech.wetech.weshop.po.Footprint;
 
 /**
  * 用户足迹事件监听
+ *
  * @author cjbi
  */
 @Component
@@ -20,6 +22,7 @@ public class FootprintListener {
     private FootprintMapper footprintMapper;
 
     @EventListener
+    @TransactionalEventListener
     public void handleFootprintEvent(Footprint footprint) {
         logger.info("Handling footprint event.", footprint);
         footprintMapper.insertSelective(footprint);
