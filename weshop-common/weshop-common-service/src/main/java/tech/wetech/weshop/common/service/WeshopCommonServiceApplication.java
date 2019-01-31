a88545b2abd19be7610dde455fb048566ca5c095
Jinbao Cheng <cjbi@outlook.com>
2019-01-31 15:34:59 +0800

    微服务化代码

--- weshop-common/weshop-common-service/src/main/java/tech/wetech/weshop/common/service/WeshopCommonServiceApplication.java
+++ weshop-common/weshop-common-service/src/main/java/tech/wetech/weshop/common/service/WeshopCommonServiceApplication.java
@@ -1,14 +1,14 @@
 package tech.wetech.weshop.common.service;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringCloudApplication
 public class WeshopCommonServiceApplication {
 
     public static void main(String[] args) {
@@ -19,6 +19,16 @@ public class WeshopCommonServiceApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
