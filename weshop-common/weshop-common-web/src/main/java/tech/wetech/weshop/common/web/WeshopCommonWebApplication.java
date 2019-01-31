cc48286f40483a7d144670356ba3b8e5d1f5193f
Jinbao Cheng <cjbi@outlook.com>
2019-01-31 11:30:50 +0800

    微服务化代码

--- weshop-common/weshop-common-web/src/main/java/tech/wetech/weshop/common/web/WeshopCommonWebApplication.java
+++ weshop-common/weshop-common-web/src/main/java/tech/wetech/weshop/common/web/WeshopCommonWebApplication.java
@@ -1,12 +1,10 @@
 package tech.wetech.weshop.common.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringCloudApplication
 @EnableFeignClients
 public class WeshopCommonWebApplication {
 
