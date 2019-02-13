e259b279eec22a5cc604c648f1f1d38268384379
Jinbao Cheng <cjbi@outlook.com>
2019-02-13 10:58:44 +0800

    集成配置中心

--- weshop-example/src/main/java/tech/wetech/weshop/example/TestController.java
+++ weshop-example/src/main/java/tech/wetech/weshop/example/TestController.java
@@ -1,27 +1,16 @@
-package tech.wetech.weshop.common.web.controller;
+package tech.wetech.weshop.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import tech.wetech.weshop.common.web.rest.TestClient;
 
 @RestController
 @RefreshScope
 public class TestController {
 
-    @Autowired
-    private TestClient testClient;
-
     @Value("${aaa}")
     private String aaa;
-
-    @GetMapping("/hello/{name}")
-    public String sayHello(@PathVariable String name) {
-        return testClient.sayHello(name);
-    }
 
     @GetMapping("/from")
     public String from() {
