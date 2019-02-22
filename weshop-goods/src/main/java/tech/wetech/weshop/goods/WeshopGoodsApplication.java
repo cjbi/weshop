cd385e29de0f4a7cec1a18de035ae592872e4dc0
Jinbao Cheng <cjbi@outlook.com>
2019-02-22 16:53:51 +0800

    修改配置

--- weshop-goods/src/main/java/tech/wetech/weshop/goods/WeshopGoodsApplication.java
+++ weshop-goods/src/main/java/tech/wetech/weshop/goods/WeshopGoodsApplication.java
@@ -6,13 +6,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringCloudApplication
 @EnableWebMvc
 @EnableSwagger2
 @ComponentScan("tech.wetech.weshop")
-@MapperScan(basePackages = "tech.wetech.weshop.goods.mapper")
 @EnableFeignClients("tech.wetech.weshop.*.api")
 public class WeshopGoodsApplication {
 
