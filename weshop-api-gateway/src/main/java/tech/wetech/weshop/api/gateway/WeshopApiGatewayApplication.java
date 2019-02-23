1dcd19be875444dbaad16e70337de14234762e65
Jinbao Cheng <cjbi@outlook.com>
2019-02-23 15:56:06 +0800

    更新依赖版本到最新版，设置网关超时配置

--- weshop-api-gateway/src/main/java/tech/wetech/weshop/api/gateway/WeshopApiGatewayApplication.java
+++ weshop-api-gateway/src/main/java/tech/wetech/weshop/api/gateway/WeshopApiGatewayApplication.java
@@ -1,15 +1,56 @@
 package tech.wetech.weshop.api.gateway;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableZuulProxy
+@EnableWebMvc
+@EnableSwagger2
+@ComponentScan("tech.wetech.weshop")
 @SpringCloudApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 public class WeshopApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeshopApiGatewayApplication.class, args);
+    }
+
+    @Component
+    @Primary
+    class DocumentationConfig implements SwaggerResourcesProvider {
+        @Override
+        public List<SwaggerResource> get() {
+            List resources = new ArrayList<>();
+            resources.add(swaggerResource("weshop-user", "/weshop-user/v2/api-docs", "1.0"));
+            resources.add(swaggerResource("weshop-goods", "/weshop-goods/v2/api-docs", "1.0"));
+            resources.add(swaggerResource("weshop-marketing", "/weshop-marketing/v2/api-docs", "1.0"));
+            resources.add(swaggerResource("weshop-order", "/weshop-order/v2/api-docs", "1.0"));
+            resources.add(swaggerResource("weshop-wechat", "/weshop-wechat/v2/api-docs", "1.0"));
+            resources.add(swaggerResource("weshop-admin", "/weshop-admin/v2/api-docs", "1.0"));
+            return resources;
+        }
+
+        private SwaggerResource swaggerResource(String name, String location, String version) {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion(version);
+            return swaggerResource;
+        }
     }
 
 }
--- weshop-framework/src/main/java/tech/wetech/weshop/enums/ResultCodeEnum.java
+++ weshop-framework/src/main/java/tech/wetech/weshop/enums/ResultCodeEnum.java
@@ -6,21 +6,21 @@ package tech.wetech.weshop.enums;
 public enum ResultCodeEnum {
 
     OK("200", "处理成功"),
-    BAD_REQUEST("400", "请求参数有误"),
-    UNAUTHORIZED("401", "未授权"),
-    PARAMS_MISS("483", "缺少接口中必填参数"),
-    PARAM_ERROR("484", "参数非法"),
-    FAILED_DEL_OWN("485", "不能删除自己"),
-    FAILED_USER_ALREADY_EXIST("486", "该用户已存在"),
-    INTERNAL_SERVER_ERROR("500", "服务器内部错误"),
-    NOT_IMPLEMENTED("501", "业务异常"),
-    RECORD_NOT_EXIST("502", "记录不存在"),
-    REMOTE_SERVICE_ERROR("503", "远程服务调用失败"),
+    BAD_REQUEST("600", "请求参数有误"),
+    UNAUTHORIZED("601", "未授权"),
+    PARAMS_MISS("602", "缺少接口中必填参数"),
+    PARAM_ERROR("603", "参数非法"),
+    FAILED_DEL_OWN("604", "不能删除自己"),
+    FAILED_USER_ALREADY_EXIST("605", "该用户已存在"),
+    INTERNAL_SERVER_ERROR("606", "服务器内部错误"),
+    NOT_IMPLEMENTED("607", "业务异常"),
+    RECORD_NOT_EXIST("608", "记录不存在"),
+    REMOTE_SERVICE_ERROR("609", "远程服务调用失败"),
 
-    GOODS_HAVE_BEEN_TAKEN_OFF_THE_SHELVES("600", "商品已下架"),
-    UNDER_STOCK("601", "库存不足"),
-    PLEASE_SELECT_GOODS("602", "请选择商品"),
-    PLEASE_SELECT_SHIPPING_ADDRESS("603", "请选择收货地址");
+    GOODS_HAVE_BEEN_TAKEN_OFF_THE_SHELVES("610", "商品已下架"),
+    UNDER_STOCK("611", "库存不足"),
+    PLEASE_SELECT_GOODS("612", "请选择商品"),
+    PLEASE_SELECT_SHIPPING_ADDRESS("613", "请选择收货地址");
     private String code;
     private String msg;
 
